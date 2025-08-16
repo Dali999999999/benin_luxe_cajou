1ab1aa31f0f6aaaadc5617bfb1dfe42e06b84840
Dali999999999 <dalinigba@gmail.com>
2025-08-16 08:47:09 +0100

    Create routes.py

---
+++ benin_luxe_cajou_api/app/public_api/routes.py
@@ -0,0 +1,32 @@
+# app/public_api/routes.py
+
+from flask import Blueprint, jsonify
+from app.models import Categorie, Produit
+from app.schemas import categories_schema, produits_schema, produit_schema
+
+public_api_bp = Blueprint('public_api', __name__)
+
+@public_api_bp.route('/categories', methods=['GET'])
+def get_public_categories():
+    """
+    Retourne la liste de toutes les catégories ACTIVES pour le site client.
+    """
+    categories = Categorie.query.filter_by(statut='actif').all()
+    return jsonify(categories_schema.dump(categories)), 200
+
+@public_api_bp.route('/products', methods=['GET'])
+def get_public_products():
+    """
+    Retourne la liste de tous les produits ACTIFS pour le site client.
+    (Pourrait être amélioré plus tard avec de la pagination)
+    """
+    produits = Produit.query.filter_by(statut='actif').order_by(Produit.id.desc()).all()
+    return jsonify(produits_schema.dump(produits)), 200
+
+@public_api_bp.route('/products/<int:id>', methods=['GET'])
+def get_public_product_detail(id):
+    """
+    Retourne les détails d'un seul produit ACTIF.
+    """
+    produit = Produit.query.filter_by(id=id, statut='actif').first_or_404()
+    return jsonify(produit_schema.dump(produit)), 200
