016b95cdfbd57e4247c80f6bd10b9ade56e4068b
Dali999999999 <dalinigba@gmail.com>
2025-08-18 18:44:14 +0100

    Update schemas.py

--- benin_luxe_cajou_api/app/schemas.py
+++ benin_luxe_cajou_api/app/schemas.py
@@ -3,21 +3,21 @@
 from .extensions import ma
 from .models import (
     Categorie, TypeProduit, Produit, ImageProduit, Panier, 
-    Utilisateur, AdresseLivraison, Commande, ZoneLivraison, Coupon, DetailsCommande, SuiviCommande,
+    Utilisateur, AdresseLivraison, Commande, DetailsCommande, SuiviCommande,
+    ZoneLivraison, Coupon
 )
 
-# --- CORRECTION : On définit SimpleTypeProduitSchema AVANT CategorieSchema ---
+# --- SCHÉMA UTILITAIRE (Défini en premier pour être utilisé par CategorieSchema) ---
 
 class SimpleTypeProduitSchema(ma.SQLAlchemyAutoSchema):
     """Schéma simplifié pour la structure du catalogue, évite les dépendances circulaires."""
     class Meta:
         model = TypeProduit
-        # On ne prend que les champs strictement nécessaires pour l'UI de navigation
         fields = ("id", "nom", "image_url") 
         load_instance = True
 
 # -----------------------------------------------------------------------------
-# DÉFINITIONS DES SCHÉMAS
+# DÉFINITIONS DES SCHÉMAS PRINCIPAUX
 # -----------------------------------------------------------------------------
 
 class ImageProduitSchema(ma.SQLAlchemyAutoSchema):
@@ -30,8 +30,7 @@ class ImageProduitSchema(ma.SQLAlchemyAutoSchema):
 class CategorieSchema(ma.SQLAlchemyAutoSchema):
     date_creation = ma.auto_field()
     date_modification = ma.auto_field()
-    # Maintenant, CategorieSchema peut utiliser SimpleTypeProduitSchema car il est déjà défini
-    types_produits = ma.Nested(SimpleTypeProduitSchema, many=True)
+    types_produits = ma.Nested(SimpleTypeProduitSchema, many=True, dump_only=True)
     class Meta:
         model = Categorie
         load_instance = True
@@ -107,17 +106,34 @@ class CouponSchema(ma.SQLAlchemyAutoSchema):
         model = Coupon
         load_instance = True
 
+# --- SCHÉMAS DÉDIÉS AU DÉTAIL DE LA COMMANDE ---
+
 class DetailsCommandeSchema(ma.SQLAlchemyAutoSchema):
-    # Inclure les détails du produit pour chaque ligne de la commande
-    produit = ma.Nested(ProduitSchema(only=("nom", "quantite_contenant", "type_contenant")))
+    """Schéma pour un article dans le détail d'une commande."""
+    produit = ma.Nested(ProduitSchema(only=("nom", "images"))) # On inclut l'image pour l'UI
+    prix_unitaire = ma.auto_field(as_string=True)
+    sous_total = ma.auto_field(as_string=True)
     class Meta:
         model = DetailsCommande
         load_instance = True
 
-class CommandeSchema(ma.SQLAlchemyAutoSchema):
+class SuiviCommandeSchema(ma.SQLAlchemyAutoSchema):
+    """Schéma pour une étape du suivi de livraison."""
+    date_changement = ma.auto_field()
+    class Meta:
+        model = SuiviCommande
+        load_instance = True
+        exclude = ("modifie_par",) # L'ID de l'admin n'est pas utile pour le client
+
+class CommandeDetailSchema(ma.SQLAlchemyAutoSchema):
+    """Schéma complet pour la page de détail d'une commande (côté client et admin)."""
+    # Informations sur le client (backref depuis le modèle Commande)
     client = ma.Nested(UtilisateurSchema(only=("prenom", "nom", "email", "telephone")))
+    # Données imbriquées
     adresse_livraison = ma.Nested(AdresseLivraisonSchema)
     details = ma.Nested(DetailsCommandeSchema, many=True)
+    suivi = ma.Nested(SuiviCommandeSchema, many=True)
+    # Champs financiers formatés
     total = ma.auto_field(as_string=True)
     sous_total = ma.auto_field(as_string=True)
     frais_livraison = ma.auto_field(as_string=True)
@@ -129,24 +145,19 @@ class CommandeSchema(ma.SQLAlchemyAutoSchema):
         include_fk = True
 
 # -----------------------------------------------------------------------------
-# INITIALISATION GLOBALE
+# INITIALISATION GLOBALE DE TOUS LES SCHÉMAS
 # -----------------------------------------------------------------------------
 categorie_schema, categories_schema = CategorieSchema(), CategorieSchema(many=True)
 type_produit_schema, types_produits_schema = TypeProduitSchema(), TypeProduitSchema(many=True)
 produit_schema, produits_schema = ProduitSchema(), ProduitSchema(many=True)
 image_produit_schema = ImageProduitSchema()
 panier_schema, paniers_schema = PanierSchema(), PanierSchema(many=True)
-utilisateur_schema = UtilisateurSchema()
-utilisateurs_schema = UtilisateurSchema(many=True)
+utilisateur_schema, utilisateurs_schema = UtilisateurSchema(), UtilisateurSchema(many=True)
 adresse_livraison_schema, adresses_livraison_schema = AdresseLivraisonSchema(), AdresseLivraisonSchema(many=True)
 commande_summary_schema, commandes_summary_schema = CommandeSummarySchema(), CommandeSummarySchema(many=True)
 zone_livraison_schema, zones_livraison_schema = ZoneLivraisonSchema(), ZoneLivraisonSchema(many=True)
 coupon_schema, coupons_schema = CouponSchema(), CouponSchema(many=True)
-commande_schema = CommandeSchema()
-commandes_schema = CommandeSchema(many=True, only=("id", "numero_commande", "client.prenom", "client.nom", "total", "statut", "date_commande"))
-
-
-
-
-
-
+# Schéma pour la liste des commandes admin (optimisé)
+commandes_schema = CommandeDetailSchema(many=True, only=("id", "numero_commande", "client.prenom", "client.nom", "total", "statut", "date_commande"))
+# Schéma pour le détail d'une commande (côté client et admin)
+commande_detail_schema = CommandeDetailSchema()
