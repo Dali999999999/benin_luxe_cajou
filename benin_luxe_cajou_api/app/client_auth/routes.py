6b9625f00667408c9b8747d0911c44952f708125
Dali999999999 <dalinigba@gmail.com>
2025-08-20 14:22:24 +0100

    Update routes.py

--- benin_luxe_cajou_api/app/client_auth/routes.py
+++ benin_luxe_cajou_api/app/client_auth/routes.py
@@ -1,7 +1,7 @@
 # app/client_auth/routes.py
 
 from flask import Blueprint, request, jsonify, current_app
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from flask_mail import Message
 import secrets
 
